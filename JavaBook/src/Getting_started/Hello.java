4a8d113b1cb55b373f77b4bccc3e954608f3698b
gimsohyeong <gimsohyeong@gmail.com>
2016-12-27 18:12:20 +0900

    commit

--- JavaBook/src/Getting_started/Hello.java
+++ JavaBook/src/Getting_started/Hello.java
@@ -2,6 +2,6 @@ package Getting_started;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, world."); //화면에 글자를 출력한다.
+		System.out.println("Hello, world.1"); //화면에 글자를 출력한다.
 	}
 }
